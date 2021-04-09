e95239759fdb48b50cf1053bcf9e836fe0605885
Felipe Lemos <felipe.lemos@bluestone.co.uk>
2021-04-09 17:09:56 +0100

    Delete junk

--- samples/BasicConsoleApp/Migrations2/Configuration.cs
+++
@@ -1,9 +0,0 @@
-using System.Data.Entity.Migrations;
-using BasicConsoleApp.Models;
-
-namespace BasicConsoleApp.Migrations2
-{
-	public class Configuration : DbMigrationsConfiguration<AnotherDbContext>
-	{
-	}
-}
